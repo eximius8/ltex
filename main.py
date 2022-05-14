c300d45c8947a37bdc4076b68c02c09d8e5e4046
eximius8 <mikhail.trunov@gmail.com>
2022-05-14 15:07:20 +0300

    Update main

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ mytext = mytext.replace('µ', '\mu ')
 mytext = mytext.replace('ℱ', 'F')
 mytext = mytext.replace('≡', '\equiv')
 mytext = mytext.replace('→', '\rightarrow')
-
+mytext = mytext.replace('≤', '\leq')
+mytext = mytext.replace('≥', '\geq')
+mytext = mytext.replace('ϰ', '\kappa')
 
 
 mytext = mytext.replace('⊇', '\supseteq')
@@ -15,12 +17,16 @@ mytext = mytext.replace('DnR', 'D_{nR}')
 mytext = mytext.replace('∃', '\exists')
 mytext = mytext.replace('D1R', 'D_{1R}')
 
+mytext = mytext.replace('\n', '')
+mytext = mytext.replace('\n', '')
+
+
 
 
 
 for i in range(22, 0, -1):
     for j in range(22, 0, -1):
-        for Letter in ['F', 'E', 'D']:
+        for Letter in ['F', 'E', 'D', '\kappa']:
             mytext = mytext.replace(f'{Letter}{i},{j}', f'{Letter}_' + '{' + f'{i},{j}' + '}')
             
         for k in range(22, 0, -1):
@@ -33,7 +39,7 @@ for Main in ['F', 'E']:
             mytext = mytext.replace(f'{Main}{ind},{sind}', f'{Main}_' + '{' + f'{ind},{sind}' + '}'  )
             mytext = mytext.replace(f'{Main}{ind}{sind}', f'{Main}_' + '{' + f'{ind}{sind}' + '}'  )
 
-for Main in ['F', 'E', 'D']:
-    for ind in ['i', 'j', 'k', 'n', '1', 'm']:
+for Main in ['F', 'E', 'D', '\kappa']:
+    for ind in ['i', 'j', 'k', 'n', '1', 'm', '2', '3', '4']:
         mytext = mytext.replace(f'{Main}{ind}', f'{Main}_' + '{' + f'{ind}' + '}'  )
 print(mytext)
